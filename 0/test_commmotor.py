33956064f481271825e27517d6b23891e6920ff9
Prabir01 <prabir06.prs@gmail.com>
2025-05-06 17:45:19 +0530

    Trying to commit from pycharm

--- 0/test_commmotor.py
+++ 0/test_commmotor.py
@@ -13,7 +13,7 @@ from PageObject.CM_Cancel import CM_Cancel
 
 
 def test_commmotor(browserinstance,Environment_selection):
-    sleep(10)
+    sleep(2)
     #browser invocation
     driver = browserinstance
     #environment_URL=Environment_selection
